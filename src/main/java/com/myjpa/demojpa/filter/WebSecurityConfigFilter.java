bddb90f91c952300ec6ff14d4dde149353ce180c
chapparavi22 <ravi.ch222@yahoo.com>
2021-12-19 21:18:54 -0500

    Added webflux security for Rest Services

---
+++ src/main/java/com/myjpa/demojpa/filter/WebSecurityConfigFilter.java
@@ -0,0 +1,49 @@
+package com.myjpa.demojpa.filter;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebFluxSecurity
+public class WebSecurityConfigFilter  {
+
+    @Value("${spha.username}")
+    private String username;
+    @Value("${spha.passwd}")
+    private String passwd;
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(passwd)
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+            .authorizeExchange(exchanges ->
+               exchanges
+                .anyExchange().authenticated()
+            )
+            .httpBasic(withDefaults())
+            .formLogin(withDefaults())
+             //.csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()));
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+}

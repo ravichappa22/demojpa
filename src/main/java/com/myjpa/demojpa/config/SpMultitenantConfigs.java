a55293d42a43cf9e8c413d77b8240153310b19d7
chapparavi22 <ravi.ch222@yahoo.com>
2022-01-05 15:50:55 -0500

    removed unnecessary imports

--- src/main/java/com/myjpa/demojpa/config/SpMultitenantConfigs.java
+++ src/main/java/com/myjpa/demojpa/config/SpMultitenantConfigs.java
@@ -3,11 +3,8 @@ package com.myjpa.demojpa.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.scheduling.quartz.SimpleThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
